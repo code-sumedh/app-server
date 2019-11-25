6b39054099f247de16bb65f2237e8aec6ef85a13
Sumedh <sumedhkulkarni18@gmail.com>
2019-11-25 20:53:43 +0530

    Unused import removed

--- src/main/java/com/empapp/socgen/controller/Controller.java
+++ src/main/java/com/empapp/socgen/controller/Controller.java
@@ -3,7 +3,6 @@ package com.empapp.socgen.controller;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
